28893ecca9643419da131e40afb23c7e4560a30d
Selman Eser <basmimarsinan@hotmail.com>
2014-02-22 18:56:04 +0100

    Category management.

--- library/modules/category/category.source.php
+++ library/modules/category/category.source.php
@@ -15,8 +15,162 @@ if (!defined('CORE'))
 
 function category_main()
 {
+	global $core;
+
+	$actions = array('list', 'edit', 'delete');
+
+	$core['current_action'] = 'list';
+	if (!empty($_REQUEST['action']) && in_array($_REQUEST['action'], $actions))
+		$core['current_action'] = $_REQUEST['action'];
+
+	call_user_func($core['current_module'] . '_' . $core['current_action']);
+}
+
+function category_list()
+{
 	global $core, $template;
 
-	$template['page_title'] = 'Category';
-	$core['current_template'] = 'category_main';
+	$request = db_query("
+		SELECT id_category, name,
+		FROM category
+		ORDER BY name");
+	$template['categories'] = array();
+	while ($row = db_fetch_assoc($request))
+	{
+		$template['categories'][] = array(
+			'id' => $row['id_category'],
+			'name' => $row['name'],
+		);
+	}
+	db_free_result($request);
+
+	$template['page_title'] = 'Category List';
+	$core['current_template'] = 'category_list';
+}
+
+function category_edit()
+{
+	global $core, $template;
+
+	$id_category = !empty($_REQUEST['category']) ? (int) $_REQUEST['category'] : 0;
+	$is_new = empty($id_category);
+
+	if (!empty($_POST['save']))
+	{
+		check_session('category');
+
+		$values = array();
+		$fields = array(
+			'name' => 'string',
+		);
+
+		foreach ($fields as $field => $type)
+		{
+			if ($type === 'string')
+				$values[$field] = !empty($_POST[$field]) ? htmlspecialchars($_POST[$field], ENT_QUOTES) : '';
+		}
+
+		if ($values['name'] === '')
+			fatal_error('Category name field cannot be empty!');
+
+		if ($is_new)
+		{
+			$insert = array();
+			foreach ($values as $field => $value)
+				$insert[$field] = "'" . $value . "'";
+
+			db_query("
+				INSERT INTO category
+					(" . implode(', ', array_keys($insert)) . ")
+				VALUES
+					(" . implode(', ', $insert) . ")");
+		}
+		else
+		{
+			$update = array();
+			foreach ($values as $field => $value)
+				$update[] = $field . " = '" . $value . "'";
+
+			db_query("
+				UPDATE category
+				SET " . implode(', ', $update) . "
+				WHERE id_category = $id_category
+				LIMIT 1");
+		}
+	}
+
+	if (!empty($_POST['save']) || !empty($_POST['cancel']))
+		redirect(build_url('category'));
+
+	if ($is_new)
+	{
+		$template['category'] = array(
+			'is_new' => true,
+			'id' => 0,
+			'name' => '',
+		);
+	}
+	else
+	{
+		$request = db_query("
+			SELECT id_category, name
+			FROM category
+			WHERE id_category = $id_category
+			LIMIT 1");
+		while ($row = db_fetch_assoc($request))
+		{
+			$template['category'] = array(
+				'is_new' => false,
+				'id' => $row['id_category'],
+				'name' => $row['name'],
+			);
+		}
+		db_free_result($request);
+
+		if (!isset($template['category']))
+			fatal_error('The category requested does not exist!');
+	}
+
+	$template['page_title'] = (!$is_new ? 'Edit' : 'Add') . ' Category';
+	$core['current_template'] = 'category_edit';
+}
+
+function category_delete()
+{
+	global $core, $template;
+
+	$id_category = !empty($_REQUEST['category']) ? (int) $_REQUEST['category'] : 0;
+
+	$request = db_query("
+		SELECT id_category, name
+		FROM category
+		WHERE id_category = $id_category
+		LIMIT 1");
+	while ($row = db_fetch_assoc($request))
+	{
+		$template['category'] = array(
+			'id' => $row['id_category'],
+			'name' => $row['name'],
+		);
+	}
+	db_free_result($request);
+
+	if (!isset($template['category']))
+		fatal_error('The category requested does not exist!');
+
+	if (!empty($_POST['delete']))
+	{
+		check_session('category');
+
+		db_query("
+			DELETE FROM category
+			WHERE id_category = $id_category
+			LIMIT 1");
+	}
+
+	if (!empty($_POST['delete']) || !empty($_POST['cancel']))
+		redirect(build_url('category'));
+
+	$template['page_title'] = 'Delete Category';
+	$core['current_template'] = 'category_delete';
 }
--- library/modules/category/category.template.php
+++ library/modules/category/category.template.php
@@ -13,6 +13,90 @@
 if (!defined('CORE'))
 	exit();
 
-function template_category_main()
+function template_category_list()
 {
+	global $template;
+
+	echo '
+		<div class="page-header">
+			<div class="pull-right">
+				<a class="btn btn-warning" href="', build_url(array('category', 'edit')), '">Add Category</a>
+			</div>
+			<h2>Category List</h2>
+		</div>
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>';
+
+	if (empty($template['categories']))
+	{
+		echo '
+				<tr>
+					<td class="align_center" colspan="2">There are not any categories added yet!</td>
+				</tr>';
+	}
+
+	foreach ($template['categories'] as $category)
+	{
+		echo '
+				<tr>
+					<td>', $category['name'], '</td>
+					<td class="span3 align_center">
+						<a class="btn btn-primary" href="', build_url(array('category', 'edit', $category['id'])), '">Edit</a>
+						<a class="btn btn-danger" href="', build_url(array('category', 'delete', $category['id'])), '">Delete</a>
+					</td>
+				</tr>';
+	}
+
+	echo '
+			</tbody>
+		</table>';
+}
+
+function template_category_edit()
+{
+	global $user, $template;
+
+	echo '
+		<form class="form-horizontal" action="', build_url(array('category', 'edit')), '" method="post">
+			<fieldset>
+				<legend>', (!$template['category']['is_new'] ? 'Edit' : 'Add'), ' Category</legend>
+				<div class="control-group">
+					<label class="control-label" for="name">Name:</label>
+					<div class="controls">
+						<input type="text" class="input-xlarge" id="name" name="name" value="', $template['category']['name'], '" />
+					</div>
+				</div>
+				<div class="form-actions">
+					<input type="submit" class="btn btn-primary" name="save" value="Save changes" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
+				</div>
+			</fieldset>
+			<input type="hidden" name="category" value="', $template['category']['id'], '" />
+			<input type="hidden" name="session_id" value="', $user['session_id'], '" />
+		</form>';
+}
+
+function template_category_delete()
+{
+	global $user, $template;
+
+	echo '
+		<form class="form-horizontal" action="', build_url(array('category', 'delete')), '" method="post">
+			<fieldset>
+				<legend>Delete Category</legend>
+				Are you sure you want to delete the category &quot;', $template['category']['name'], '&quot;?
+				<div class="form-actions">
+					<input type="submit" class="btn btn-danger" name="delete" value="Delete" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
+				</div>
+			</fieldset>
+			<input type="hidden" name="category" value="', $template['category']['id'], '" />
+			<input type="hidden" name="session_id" value="', $user['session_id'], '" />
+		</form>';
 }
