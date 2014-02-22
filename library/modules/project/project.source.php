bc72ecc860a3fd44ed096dcebd7c1b0986dc0fac
Selman Eser <basmimarsinan@hotmail.com>
2014-02-22 18:56:14 +0100

    Project management.

--- library/modules/project/project.source.php
+++ library/modules/project/project.source.php
@@ -15,8 +15,188 @@ if (!defined('CORE'))
 
 function project_main()
 {
+	global $core;
+
+	$actions = array('list', 'edit', 'delete');
+
+	$core['current_action'] = 'list';
+	if (!empty($_REQUEST['action']) && in_array($_REQUEST['action'], $actions))
+		$core['current_action'] = $_REQUEST['action'];
+
+	call_user_func($core['current_module'] . '_' . $core['current_action']);
+}
+
+function project_list()
+{
 	global $core, $template;
 
-	$template['page_title'] = 'Project';
-	$core['current_template'] = 'project_main';
+	$request = db_query("
+		SELECT p.id_project, p.name, c.name AS category
+		FROM project AS p
+			LEFT JOIN category AS c ON (c.id_category = p.id_category)
+		ORDER BY category, name");
+	$template['projects'] = array();
+	while ($row = db_fetch_assoc($request))
+	{
+		$template['projects'][] = array(
+			'id' => $row['id_project'],
+			'name' => $row['name'],
+			'category' => $row['category'],
+		);
+	}
+	db_free_result($request);
+
+	$template['page_title'] = 'Project List';
+	$core['current_template'] = 'project_list';
+}
+
+function project_edit()
+{
+	global $core, $template;
+
+	$id_project = !empty($_REQUEST['project']) ? (int) $_REQUEST['project'] : 0;
+	$is_new = empty($id_project);
+
+	if ($is_new)
+	{
+		$template['project'] = array(
+			'is_new' => true,
+			'id' => 0,
+			'category' => 0,
+			'name' => '',
+		);
+	}
+	else
+	{
+		$request = db_query("
+			SELECT id_project, id_category, name
+			FROM project
+			WHERE id_project = $id_project
+			LIMIT 1");
+		while ($row = db_fetch_assoc($request))
+		{
+			$template['project'] = array(
+				'is_new' => false,
+				'id' => $row['id_project'],
+				'category' => $row['id_category'],
+				'name' => $row['name'],
+			);
+		}
+		db_free_result($request);
+
+		if (!isset($template['project']))
+			fatal_error('The project requested does not exist!');
+	}
+
+	if (!empty($_POST['save']))
+	{
+		check_session('project');
+
+		$values = array();
+		$fields = array(
+			'id_category' => 'int',
+			'name' => 'string',
+		);
+
+		foreach ($fields as $field => $type)
+		{
+			if ($type === 'string')
+				$values[$field] = !empty($_POST[$field]) ? htmlspecialchars($_POST[$field], ENT_QUOTES) : '';
+			elseif ($type === 'int')
+				$values[$field] = !empty($_POST[$field]) ? (int) $_POST[$field] : 0;
+		}
+
+		if ($values['name'] === '')
+			fatal_error('Project name field cannot be empty!');
+		elseif ($values['id_category'] === 0)
+			fatal_error('Project category field cannot be empty!');
+
+		if ($is_new)
+		{
+			$insert = array();
+			foreach ($values as $field => $value)
+				$insert[$field] = "'" . $value . "'";
+
+			db_query("
+				INSERT INTO project
+					(" . implode(', ', array_keys($insert)) . ")
+				VALUES
+					(" . implode(', ', $insert) . ")");
+		}
+		else
+		{
+			$update = array();
+			foreach ($values as $field => $value)
+				$update[] = $field . " = '" . $value . "'";
+
+			db_query("
+				UPDATE project
+				SET " . implode(', ', $update) . "
+				WHERE id_project = $id_project
+				LIMIT 1");
+		}
+	}
+
+	if (!empty($_POST['save']) || !empty($_POST['cancel']))
+		redirect(build_url('project'));
+
+	$request = db_query("
+		SELECT id_category, name
+		FROM category
+		ORDER BY name");
+	$template['categories'] = array();
+	while ($row = db_fetch_assoc($request))
+	{
+		$template['categories'][] = array(
+			'id' => $row['id_category'],
+			'name' => $row['name'],
+		);
+	}
+	db_free_result($request);
+
+	if (empty($template['categories']))
+		fatal_error('There are no categories added yet! You cannot add projects without categories!');
+
+	$template['page_title'] = (!$is_new ? 'Edit' : 'Add') . ' Project';
+	$core['current_template'] = 'project_edit';
+}
+
+function project_delete()
+{
+	global $core, $template;
+
+	$id_project = !empty($_REQUEST['project']) ? (int) $_REQUEST['project'] : 0;
+
+	$request = db_query("
+		SELECT id_project, name
+		FROM project
+		WHERE id_project = $id_project
+		LIMIT 1");
+	while ($row = db_fetch_assoc($request))
+	{
+		$template['project'] = array(
+			'id' => $row['id_project'],
+			'name' => $row['name'],
+		);
+	}
+	db_free_result($request);
+
+	if (!isset($template['project']))
+		fatal_error('The project requested does not exist!');
+
+	if (!empty($_POST['delete']))
+	{
+		check_session('project');
+
+		db_query("
+			DELETE FROM project
+			WHERE id_project = $id_project
+			LIMIT 1");
+	}
+
+	if (!empty($_POST['delete']) || !empty($_POST['cancel']))
+		redirect(build_url('project'));
+
+	$template['page_title'] = 'Delete project';
+	$core['current_template'] = 'project_delete';
 }
--- library/modules/project/project.template.php
+++ library/modules/project/project.template.php
@@ -13,6 +13,108 @@
 if (!defined('CORE'))
 	exit();
 
-function template_project_main()
+function template_project_list()
 {
+	global $template;
+
+	echo '
+		<div class="page-header">
+			<div class="pull-right">
+				<a class="btn btn-warning" href="', build_url(array('project', 'edit')), '">Add Project</a>
+			</div>
+			<h2>Project List</h2>
+		</div>
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Category</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>';
+
+	if (empty($template['projects']))
+	{
+		echo '
+				<tr>
+					<td class="align_center" colspan="3">There are not any projects added yet!</td>
+				</tr>';
+	}
+
+	foreach ($template['projects'] as $project)
+	{
+		echo '
+				<tr>
+					<td>', $project['name'], '</td>
+					<td>', $project['category'], '</td>
+					<td class="span3 align_center">
+						<a class="btn btn-primary" href="', build_url(array('project', 'edit', $project['id'])), '">Edit</a>
+						<a class="btn btn-danger" href="', build_url(array('project', 'delete', $project['id'])), '">Delete</a>
+					</td>
+				</tr>';
+	}
+
+	echo '
+			</tbody>
+		</table>';
+}
+
+function template_project_edit()
+{
+	global $user, $template;
+
+	echo '
+		<form class="form-horizontal" action="', build_url(array('project', 'edit')), '" method="post">
+			<fieldset>
+				<legend>', (!$template['project']['is_new'] ? 'Edit' : 'Add'), ' Project</legend>
+				<div class="control-group">
+					<label class="control-label" for="name">Name:</label>
+					<div class="controls">
+						<input type="text" class="input-xlarge" id="name" name="name" value="', $template['project']['name'], '" />
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="id_category">Category:</label>
+					<div class="controls">
+						<select id="id_category" name="id_category">
+							<option value="0"', ($template['project']['category'] == 0 ? ' selected="selected"' : ''), '>Select category</option>';
+
+	foreach ($template['categories'] as $category)
+	{
+		echo '
+							<option value="', $category['id'], '"', ($template['project']['category'] == $category['id'] ? ' selected="selected"' : ''), '>', $category['name'], '</option>';
+	}
+
+	echo '
+						</select>
+					</div>
+				</div>
+				<div class="form-actions">
+					<input type="submit" class="btn btn-primary" name="save" value="Save changes" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
+				</div>
+			</fieldset>
+			<input type="hidden" name="project" value="', $template['project']['id'], '" />
+			<input type="hidden" name="session_id" value="', $user['session_id'], '" />
+		</form>';
+}
+
+function template_project_delete()
+{
+	global $user, $template;
+
+	echo '
+		<form class="form-horizontal" action="', build_url(array('project', 'delete')), '" method="post">
+			<fieldset>
+				<legend>Delete Project</legend>
+				Are you sure you want to delete the project &quot;', $template['project']['name'], '&quot;?
+				<div class="form-actions">
+					<input type="submit" class="btn btn-danger" name="delete" value="Delete" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
+				</div>
+			</fieldset>
+			<input type="hidden" name="project" value="', $template['project']['id'], '" />
+			<input type="hidden" name="session_id" value="', $user['session_id'], '" />
+		</form>';
 }
